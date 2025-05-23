0e6a6c666f83a54cb6665acd84202b006fa04af5
Eugene-E <clinpop796@gmail.com>
2025-05-23 21:03:31 +0300

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.getElementById('nav-toggle').addEventListener('click', function () {
   document.querySelector('.navbar ul').classList.toggle('open');
 });
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.1,
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('visible');
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
